1429b28db0fb9e2253f2d0205840914246ba82b8
Eric <eric-noren@hotmail.com>
2017-11-16 13:06:33 +0100

    Stuff

---
+++ BehaviourEditor/Assets/Scripts/TankBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBehaviour : MonoBehaviour {
+
+    // Publics
+    public float speed = 1.0f;
+
+    // Privates
+    private Rigidbody2D body;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        body = GetComponent<Rigidbody2D>();
+	}
+
+    public void moveForward()
+    {
+        body.velocity = transform.up * speed;
+    }
+    public void moveBackwards()
+    {
+        body.velocity = transform.up * speed * -1;
+    }
+
+    // FOR TESTING PURPOSES
+    void Update()
+    {
+        moveForward();
+        print(transform.up);
+    }
+
+}
